2fe73dee19411cb693daa875535f0c1f8361ff3c
Diana <dianabacioccoart@gmail.com>
2021-10-18 19:00:01 -0700

    commit tree ecb615631b3094ba59d1271fdc85fdfc23716ec7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+  document.addEventListener("DOMContentLoaded",() => {
+    let newWords = document.getElementById('text');
+    newWords.textContent = 'This is really cool!'
+    console.log('newWords.textContent')
+    });
